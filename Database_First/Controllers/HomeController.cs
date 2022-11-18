87edeaa9e1e5c80da04ad4f2e9bb9f555cf612e0
Lucas Juan <ljgsodre@outlook.com>
2022-11-18 15:47:40 -0300

    Changes on API to grab a Data Context

---
+++ Database_First/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Database_First.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Database_First.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("GetData")]
+        public async Task<IActionResult> GetData()
+        {
+            AppDbContext context = new AppDbContext();
+            var pessoa = context.Pessoas.FirstOrDefault();
+            pessoa.Nome = "Lucas Juan";
+            
+            return Ok(pessoa);
+
+        }
+    }
+}
